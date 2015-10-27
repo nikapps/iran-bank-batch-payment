d61e36999368de7afb7ee620a39db3f0417af397
Hossein Moradgholi <hossein.moradgholi@taxiyaab.com>
2015-10-27 13:53:27 +0330

    minor fix

--- src/Paya/PayaCollection.php
+++ src/Paya/PayaCollection.php
@@ -14,6 +14,23 @@ class PayaCollection
     /** @var  PayaPaymentInfo */
     private $payaPaymentInfo;
     private $messageId;
+    private $requestDateTime;
+
+    /**
+     * @return mixed
+     */
+    public function getRequestDateTime()
+    {
+        return $this->requestDateTime;
+    }
+
+    /**
+     * @param mixed $requestDateTime
+     */
+    public function setRequestDateTime($requestDateTime)
+    {
+        $this->requestDateTime = $requestDateTime;
+    }
 
     /**
      * @return mixed
@@ -56,70 +73,70 @@ class PayaCollection
         $writer->setIndent(true);
         // header
         $writer->startElement('CstmrCdtTrfInitn');
-            $writer->startElement('GrpHdr');
-                $writer->writeElement('MsgId', $this->getMessageId());
-                $writer->writeElement('CreDtTm', $this->getPayaPaymentInfo()->getRequestDate());
-                $writer->writeElement('NbOfTxs', $this->getPayaPaymentInfo()->getNumberOfPayments());
-                $writer->writeElement('CtrlSum', $this->getPayaPaymentInfo()->getTotalTransactionsAmount());
-                $writer->startElement('InitgPty');
-                    $writer->writeElement('Nm', $this->getPayaPaymentInfo()->getPayerName());
-                $writer->endElement();
-            $writer->endElement();
+        $writer->startElement('GrpHdr');
+        $writer->writeElement('MsgId', $this->getMessageId());
+        $writer->writeElement('CreDtTm', $this->getRequestDateTime());
+        $writer->writeElement('NbOfTxs', $this->getPayaPaymentInfo()->getNumberOfPayments());
+        $writer->writeElement('CtrlSum', $this->getPayaPaymentInfo()->getTotalTransactionsAmount());
+        $writer->startElement('InitgPty');
+        $writer->writeElement('Nm', $this->getPayaPaymentInfo()->getPayerName());
+        $writer->endElement();
         $writer->endElement();
 
         // payment info
         $writer->startElement('PmtInf');
-            $writer->writeElement('PmtInfId', $this->payaPaymentInfo->getPaymentInfoId());
+        $writer->writeElement('PmtInfId', $this->payaPaymentInfo->getPaymentInfoId());
 
-            $writer->startElement('PmtMtd');
-                $writer->writeAttribute('Ccy', 'IRR');
-                $writer->write($this->payaPaymentInfo->getPaymentMethod());
+        $writer->startElement('PmtMtd');
+        $writer->writeAttribute('Ccy', 'IRR');
+        $writer->write($this->payaPaymentInfo->getPaymentMethod());
+        $writer->endElement();
+
+        $writer->writeElement('NbOfTxs', $this->payaPaymentInfo->getNumberOfPayments());
+        $writer->writeElement('CtrlSum',$this->payaPaymentInfo->getTotalTransactionsAmount());
+        $writer->writeElement('ReqdExctnDt',$this->payaPaymentInfo->getRequestDate());
+
+        $writer->startElement('Dbtr');
+        $writer->writeElement('Nm', $this->payaPaymentInfo->getPayerName());
+        $writer->endElement();
+
+        $writer->startElement('DbtrAcct');
+        $writer->startElement('Id');
+        $writer->writeElement('IBAN', $this->payaPaymentInfo->getPayerIban());
+        $writer->endElement();
+        $writer->endElement();
+
+        $writer->startElement('DbtrAcct');
+        $writer->startElement('FinInstnId');
+        $writer->writeElement('BIC', 'BMJIIRTHXXX');
+        $writer->endElement();
+        $writer->endElement();
+
+        /** @var PayaPayment $payment */
+        foreach ($this->payaPaymentInfo->getPayments() as $payment) {
+            $writer->startElement('CdtTrfTxInf');
+            $writer->startElement('PmtId');
+            $writer->writeElement('InstrId', 'EMPTY');
+            $writer->writeElement('EndToEndId', 'EMPTY');
             $writer->endElement();
-
-            $writer->writeElement('NbOfTxs', $this->payaPaymentInfo->getNumberOfPayments());
-            $writer->writeElement('CtrlSum',$this->payaPaymentInfo->getTotalTransactionsAmount());
-            $writer->writeElement('ReqdExctnDt',$this->payaPaymentInfo->getRequestDate());
-
-            $writer->startElement('Dbtr');
-                $writer->writeElement('Nm', $this->payaPaymentInfo->getPayerName());
+            $writer->startElement('Amt');
+            $writer->startElement('InstdAmt');
+            $writer->writeAttribute('Ccy','IRR');
+            $writer->write($payment->getAmount());
             $writer->endElement();
-
-            $writer->startElement('DbtrAcct');
-                $writer->startElement('Id');
-                    $writer->writeElement('IBAN', $this->payaPaymentInfo->getPayerIban());
-                $writer->endElement();
             $writer->endElement();
-
-            $writer->startElement('DbtrAcct');
-                $writer->startElement('FinInstnId');
-                    $writer->writeElement('BIC', 'BMJIIRTHXXX');
-                $writer->endElement();
+            $writer->startElement('Cdtr');
+            $writer->writeElement('Nm', $payment->getCreditorName());
             $writer->endElement();
+            $writer->startElement('CdtrAcct');
+            $writer->startElement('Id');
+            $writer->writeElement('IBAN', $payment->getCreditorIban());
+            $writer->endElement();
+            $writer->endElement();
+            $writer->endElement();
+        }
 
-            /** @var PayaPayment $payment */
-            foreach ($this->payaPaymentInfo->getPayments() as $payment) {
-                $writer->startElement('CdtTrfTxInf');
-                    $writer->startElement('PmtId');
-                        $writer->writeElement('InstrId', 'EMPTY');
-                        $writer->writeElement('EndToEndId', 'EMPTY');
-                    $writer->endElement();
-                    $writer->startElement('Amt');
-                        $writer->startElement('InstdAmt');
-                            $writer->writeAttribute('Ccy','IRR');
-                            $writer->write($payment->getAmount());
-                        $writer->endElement();
-                    $writer->endElement();
-                    $writer->startElement('Cdtr');
-                        $writer->writeElement('Nm', $payment->getCreditorName());
-                    $writer->endElement();
-                    $writer->startElement('CdtrAcct');
-                        $writer->startElement('Id');
-                            $writer->writeElement('IBAN', $payment->getCreditorIban());
-                        $writer->endElement();
-                    $writer->endElement();
-                $writer->endElement();
-            }
-
+        $writer->endElement();
         $writer->endElement();
         $writer->endElement();
         $writer->endDocument();
